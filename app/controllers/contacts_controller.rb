a4fc33096739e2ff89f26fe7f7dd95f05c9066bf
Juan José Ramírez Calderón <juan.ramirez@tektonlabs.com>
2016-01-18 10:29:13 -0500

    Add position and extension to contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -76,6 +76,6 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:name,:email,:phone,:mobile,:birthday,:client_id)
+      params.require(:contact).permit(:name,:email,:phone,:mobile,:birthday,:client_id, :extension, :position)
     end
 end
